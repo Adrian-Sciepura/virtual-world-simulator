cff20067a67d71a78b1568b307db9fd726a70fd2
Adrian Ściepura <adriansciepura@outlook.com>
2023-04-09 10:18:48 +0200

    Added memory leak detection

--- virtual-world-simulator/virtual-world-simulator.cpp
+++ virtual-world-simulator/virtual-world-simulator.cpp
@@ -1,10 +1,20 @@
-﻿#include "SimulationManager.h"
+﻿#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#include "SimulationManager.h"
 
-int main()
+void program()
 {
 	srand(time(NULL));
 	SimulationManager simulationManager;
 	simulationManager.start();
+}
 
+int main()
+{
+	program();
+
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
