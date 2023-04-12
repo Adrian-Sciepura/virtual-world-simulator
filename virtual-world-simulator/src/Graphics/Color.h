f4d008066f04d2191ebea14967b047c0aa1db096
Adrian Ściepura <adriansciepura@outlook.com>
2023-04-13 01:13:19 +0200

    Added console colors enum

---
+++ virtual-world-simulator/src/Graphics/Color.h
@@ -0,0 +1,26 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <Windows.h>
+
+enum Color
+{
+    BLACK = 0,
+    DARKBLUE,
+    DARKGREEN,
+    DARKCYAN,
+    DARKRED,
+    DARKMAGENTA,
+    DARKYELLOW,
+    DARKGRAY,
+    GRAY,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    MAGENTA,
+    YELLOW,
+    WHITE
+};
+
+#endif
