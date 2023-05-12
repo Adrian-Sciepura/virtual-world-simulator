99a839e55b2cf4b8e38d2bc1d47d879eb6f6a017
Adrian Ściepura <adriansciepura@outlook.com>
2023-05-12 12:56:30 +0200

    Added Java files

---
+++ Java/virtual-world-simulator/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(420, 420);
+        frame.setVisible(true);
+    }
+}
